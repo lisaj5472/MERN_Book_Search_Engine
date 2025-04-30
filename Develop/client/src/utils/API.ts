f5f17f5f4de78f3192ed02dd8d2c9a201b4dc0f1
LisaJ5472 <lisaj5472@gmail.com>
2025-04-30 15:23:12 -0600

    converted all files to graphql calls

--- Develop/client/src/utils/API.ts
+++ Develop/client/src/utils/API.ts
@@ -53,8 +53,6 @@ export const deleteBook = (bookId: string, token: string) => {
   });
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query: string) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
