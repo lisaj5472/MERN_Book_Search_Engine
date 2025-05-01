5ceae7c186995cea20f634bc058fde0d51a78f7a
LisaJ5472 <lisaj5472@gmail.com>
2025-05-01 17:27:48 -0600

    changed client side build for render

--- client/src/pages/SearchBooks.tsx
+++ client/src/pages/SearchBooks.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import type { FormEvent } from "react";
 import { Container, Col, Form, Button, Card, Row } from "react-bootstrap";
 import Auth from "../utils/auth";
-import { useMutation, useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { SAVE_BOOK } from "../utils/mutations";
 import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
