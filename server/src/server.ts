517363723c4458741eacb75e68a231dd48919545
LisaJ5472 <lisaj5472@gmail.com>
2025-05-01 22:10:19 -0600

    updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,10 +31,19 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
 
     app.get("*", (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(
+        path.join(__dirname, "../../client/dist/index.html"),
+        (err) => {
+          if (err) {
+            res
+              .status(500)
+              .send("An error occurred while loading the frontend.");
+          }
+        }
+      );
     });
   }
 
